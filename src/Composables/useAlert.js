b6f836281040daf9d11d038171ad12b258b234d0
Theodore <adamtsai0408@gmail.com>
2025-02-10 20:28:48 +0800

    feat:add rollup-plugin-visualizer

--- src/Composables/useAlert.js
+++ src/Composables/useAlert.js
@@ -1,4 +1,4 @@
-import Swal from 'sweetalert2'
+import Swal from 'sweetalert2/dist/sweetalert2.js'
 
 export function useAlert() {
   const showAlert = (title, icon, time = 1000) => {
--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,21 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
-
+import { visualizer } from 'rollup-plugin-visualizer'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [vue(), vueJsx(), vueDevTools()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    vueDevTools(),
+    visualizer({
+      open: true, // 自動打開分析報告
+      filename: 'stats.html', // 生成的報告檔案名稱
+      gzipSize: true, // 顯示 gzip 後的大小
+      brotliSize: true, // 顯示 brotli 壓縮後的大小
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
